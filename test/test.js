1a369efee4ae1aff9848109410e91665ed9ce4b2
Nicolas Gallagher <nicolasgallagher@twitter.com>
2014-02-27 13:32:07 -0800

    Refactor tests
    * More test helpers.
    * Rename test fixtures.
    * Add tests for checking rules in media queries.

--- test/test.js
+++ test/test.js
@@ -3,77 +3,50 @@ var rework = require('rework');
 var expect = require('chai').expect;
 var conformance = require('..');
 
-var processFixture;
-
-function readFixture(name) {
-  return fs.readFileSync('test/fixtures/' + name + '.css', 'utf8').trim();
+function processFixture(name) {
+  var css = fs.readFileSync('test/fixtures/' + name + '.css', 'utf8').trim();
+  return rework(css).use(conformance);
 }
 
-beforeEach(function () {
-  processFixture = function (name) {
-    return rework(readFixture(name))
-      .use(conformance);
+function assertSuccess(fixture) {
+  var result = function () {
+    processFixture(fixture);
   };
-});
+  expect(result).not.to.Throw();
+}
+
+function assertFailure(fixture) {
+  var result = function () {
+    processFixture(fixture);
+  };
+  expect(result).to.Throw();
+}
 
 describe('linting', function () {
   describe('a CSS file that lacks the `@define` notation', function () {
     it('must be ignored', function () {
-      var ignore = function () {
-        processFixture('ignore');
-      };
-      expect(ignore).not.to.Throw();
+      assertSuccess('ignore');
     });
   });
 
   describe('a css file that uses the `@define` notation', function () {
-    it('must contain selectors that begin with a class', function () {
-      var result = function () {
-        processFixture('component-with-globals');
-      };
-      expect(result).to.Throw();
-    });
-
     it('must contain selectors that begin with a class matching the component name', function () {
-        processFixture('component-valid');
-      var valid = function () {
-        processFixture('component-valid');
-      };
-      expect(valid).not.to.Throw();
-
-      var invalid = function () {
-        processFixture('component-invalid');
-      };
-      expect(invalid).to.Throw();
-
-      var invalid2 = function () {
-        processFixture('component-invalid-2');
-      };
-      expect(invalid2).to.Throw();
+      assertSuccess('valid-rules');
+      assertFailure('invalid-selectors');
+      assertFailure('invalid-selectors-2');
+      assertFailure('invalid-selectors-3');
+      assertFailure('invalid-selectors-4');
     });
 
     it('must only declare custom properties, containing the component name, in a `:root` rule', function () {
-      var valid = function () {
-        processFixture('component-with-valid-root');
-      };
-      expect(valid).not.to.Throw();
-
-      var invalid = function () {
-        processFixture('component-with-invalid-root');
-      };
-      expect(invalid).to.Throw();
-
-      var invalid2 = function () {
-        processFixture('component-with-invalid-root-2');
-      };
-      expect(invalid2).to.Throw();
+      assertSuccess('valid-vars');
+      assertFailure('invalid-vars');
+      assertFailure('invalid-root');
     });
 
-    it('must handle media queries', function () {
-      var valid = function () {
-        processFixture('component-with-media-queries');
-      };
-      expect(valid).not.to.Throw();
+    it('must apply to selectors in media queries', function () {
+      assertSuccess('valid-media-query');
+      assertFailure('invalid-media-query');
     });
   });
 });
